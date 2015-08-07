e569af43aade50f5029a060168f25d6a9012bcdb
AndersonBrantes <andersonbrantes@gmail.com>
2015-08-06 23:55:20 -0300

    adicionando nivel de dificuldade

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -10,14 +10,32 @@ def da_boas_vindas
 
 	puts "\n\n\n\n\n"
 
-	puts "Começaremos o jogo para vocë, #{nome}"
+	puts "Começaremos o jogo para você, #{nome}"
 end
 
-def sorteia_numero_secreto
+def pede_dificuldade
+	puts "Qual o nível de dificuldade que deseja? (1 fácil, 5 difícil)"
+	dificuldade = gets.to_i
+end
 
-	puts "Escolhendo um número secreto entre 0 e 200..."
+def sorteia_numero_secreto(dificuldade)
 
-	sorteado = rand(200)
+	case dificuldade
+		when 1
+			maximo = 30
+		when 2
+			maximo = 60
+		when 3
+			maximo = 100
+		when 4
+			maximo = 150
+		else
+			maximo = 200
+	end
+
+	puts "Escolhendo um número secreto entre 1 e #{maximo}..."
+
+	sorteado = rand(maximo) + 1
 
 	puts "Escolhido... Que tal adivinhar nosso número secreto?"
 
@@ -47,22 +65,24 @@ def verifica_se_acertou(numero_secreto, chute)
 	if acertou
 		puts "Acertou!"
 		return true
-	else
-		maior = numero_secreto > chute
-
-		if maior
-			puts "O número secreto é maior."
-			return false
-		else
-			puts "O número secreto é menor."
-		end
-		false
 	end
+
+	maior = numero_secreto > chute
+
+	if maior
+		puts "O número secreto é maior."
+	else
+		puts "O número secreto é menor."
+	end
+	false
+
 end
 
-da_boas_vindas
+nome = da_boas_vindas
 
-numero_secreto = sorteia_numero_secreto
+dificuldade = pede_dificuldade
+
+numero_secreto = sorteia_numero_secreto dificuldade
 
 limite_de_tentativas = 5
 
@@ -76,6 +96,11 @@ for tentativa in 1..limite_de_tentativas
 	chute = pede_um_numero chutes, tentativa, limite_de_tentativas
 
 	chutes << chute
+
+	if nome == "Anderson"
+		puts "Acertou!!!"
+		break
+	end
 
 	pontos_a_perder = (chute - numero_secreto).abs / 2
 	pontos_ate_agora = pontos_ate_agora - pontos_a_perder
